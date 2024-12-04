f703a02ade21fdec0d7447c143f75655c8570674
Fizan Shamjith <fizanshamjith@gmail.com>
2024-12-04 10:45:16 +0000

    Created a navigator so that all the pages of the app are screens and not tabs

---
+++ app/navigation/AppNavigator.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../(tabs)/index'; 
+import GroupOrderScreen from '../(tabs)/group-order';
+import ExploreScreen from '../(tabs)/explore'; 
+import ProfileScreen from '../(tabs)/profile'; 
+
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="group-order" component={GroupOrderScreen} />
+        <Stack.Screen name="explore" component={ExploreScreen} />
+        <Stack.Screen name="profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
