70939918938700c300f07ca71d4e52269054df50
Aarij Khan <aarijomark@gmail.com>
2024-12-10 00:55:33 +0000

    Created a method to retrieve the connection to the database

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,9 @@ import psycopg2
 
 app = FastAPI()
 
+from models import (
+    Products, Student, GroupOrders
+)
 # Code to connect to the database from Generative AI
 def retrieveDatabaseConnection():
     return psycopg2.connect(
@@ -13,6 +16,24 @@ def retrieveDatabaseConnection():
         password='AVNS_HTFha2EWahHmllf6fuj',
         sslmode='require' 
     )
+
+def fetchingProductsFromDatabse():
+    try:
+        isConnecting = retrieveDatabaseConnection()
+        with isConnecting.cursor() as pointer:
+            pointer.execute("SELECT * FROM products")
+            return [
+                Products(id = str(row[0]), name = row[1], price = float(row[2]), category = row[3]) for row in pointer.fetchall()
+            ]
+    except Exception as e:
+        print(f"{e}")
+        return []
+    finally:
+        if 'connection' in locals():
+            isConnecting.close()
+
+          
+        
 
 
 @app.get("/")
